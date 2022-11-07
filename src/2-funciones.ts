1cfa20294844ceac92b51a4cfbb033f0534527e3
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2022-11-07 18:13:13 -0500

    Add hoisting and coertion

--- src/2-funciones.ts
+++ src/2-funciones.ts
@@ -11,3 +11,12 @@ let resta = ((a: number,b:number): number => {
 
 console.log(suma(12,123));
 console.log(resta(123,32));
+
+
+//Something with scope
+let miNombre: string = 'Fabian';
+let fullName = ((apellido: string): string =>{
+  return `${miNombre} ${apellido}`;
+});
+
+console.log(fullName('Beltrán'));
---
+++ src/3-hoisting.ts
@@ -0,0 +1,23 @@
+/**
+ * HOISTING -> Elevar, izar, alzar, levantar
+ * Es para EMScript 5 y anteriores.
+ * var nombre;
+ * console.log(nombre) => Undefined
+ * nombre = 'Fabian'
+ *
+ * Tambien hay hoisting con las funciones
+ * Solo que si llamamos una función antes de crearla, JS hace el hoisting de la función y despues si la ejecuta
+ * algo que no hace con las variables.
+ *
+ *
+ * COERCIÓN --- Forma en la que podemos cambiar un tipo de valor a otro, existen dos tipos.
+ * - COERCIÓN IMPLICITA = Cuando el lenguaje nos ayuda a cambiar el tipo de valor
+ * - COERCIÓN EXPLICITA = Cuando obligamos a que cambie el tipo de valor.
+ */
+
+
+
+
+(()=>{
+  let miNombre: string;
+})();
