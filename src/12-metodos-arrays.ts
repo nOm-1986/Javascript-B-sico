04c089de7c602b4a095dbc2750bc1e3ad99a9bc5
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2022-11-13 12:28:12 -0500

    Starting with some methods in arrays

---
+++ src/12-metodos-arrays.ts
@@ -0,0 +1,14 @@
+(() => {
+  //Array with articles
+  type Articulos = {nombre:string, costo:number};
+  let articles: Articulos[] = [
+    {nombre: 'bicicleta' , costo:1000},
+    {nombre: 'Tv' , costo:2000},
+    {nombre: 'Libro' , costo:300},
+    {nombre: 'Celular' , costo:3000},
+    {nombre: 'Laptop' , costo:5000},
+    {nombre: 'Teclado' , costo:150},
+    {nombre: 'Audifonos' , costo:100},
+    {nombre: 'Monitor' , costo:1000}
+  ]
+})();
