3095e13128ed89f164b63a7a1b8ad5178252fab0
Fabian Beltran <joshefbeltran@gmail.com>
2022-12-01 11:09:17 -0500

    starting with eventsListeners

--- src/CursoPracticoJS/script.ts
+++ src/CursoPracticoJS/script.ts
@@ -3,7 +3,7 @@
 // const parrafito: Element | null = document.querySelector('.parrafito');
 // const pid: Element | null = document.querySelector('#pid');
 // const input: HTMLInputElement | null = document.querySelector('input');
-alert('Hola mundo');
+/*alert('Hola mundo');
 const h1: HTMLHeadingElement= document.querySelector('h1') as HTMLHeadingElement;
 const p: HTMLParagraphElement | null = document.querySelector('p');
 const parrafito: Element | null = document.querySelector('.parrafito');
@@ -13,4 +13,14 @@ const input: HTMLInputElement | null = document.querySelector('input');
 //console.log({h1, p, parrafito, pid, input});
 //console.log(p);
 
-h1.innerHTML = "<h1>Tortuguita <br> rápida<h1>";
+h1.innerHTML = "<h1>Tortuguita <br> rápida<h1>";*/
+
+const input1 = document.getElementById('calculo1');
+const input2 = document.getElementById('calculo2');
+const btn1 = document.getElementById('btnCalcular');
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+});
+
+
